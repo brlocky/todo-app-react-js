27d408d5ef72863c080198dafc6ba2641e126305
Joao <brlocky@gmail.com>
2022-12-18 04:15:14 +0000

    Add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TodoPage from './pages/TodoPage';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage/>}>Home</Route>
+        <Route path="/Todo" element={<TodoPage/>}>Todo</Route>
+      </Routes>
     </div>
   );
 }
